ea80e338e727c1097936690c274c0d63edc1d07f
lebihab <dmha89@gmail.com>
2023-01-10 03:53:46 +0000

    Week13 project

--- training.py
+++ training.py
@@ -1,3 +1,16 @@
-print("Hello World")
-print("I am learning")
-print("Hello world")
+import random
+import string
+
+# Number of EC2 instances
+num_instances = int(input("Enter the number of EC2 instances: "))
+
+# Department name
+department_name = input("Enter your department name: ")
+
+# Generate unique names for the specified number of instances
+for i in range(num_instances):
+    # Generate random characters and numbers to include in the name
+    random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+    # Create the unique name
+    unique_name = department_name + '-' + random_chars
+    print(unique_name)
