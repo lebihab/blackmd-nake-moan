f511880303fe41ffe8f8ced4c3eae4c7bed1fe33
lebihab <dmha89@gmail.com>
2023-01-10 05:03:20 +0000

    training

--- traning010923.py
+++ traning010923.py
@@ -1,1 +1,14 @@
-print("Hello World")
+
+# fahrenheit = float(input("What temperature in F would you like to convert to Celcius ?"))
+#celcius = (fahrenheit - 32) * 5 / 9 
+
+# 14.5 F is -1 C
+
+#print(fahrenheit, "F is", round(celcius, 2), "C")
+
+name = input("what is you name ?")
+print(name)
+
+age = input("What is your age ?")
+
+print("my name is " + name + ", and my age is " + age)
