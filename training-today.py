4093a4e175f902ea1207dc76becc8c14304541c5
lebihab <dmha89@gmail.com>
2023-01-11 06:09:40 +0000

    Hello

--- training-today.py
+++ training-today.py
@@ -1,3 +1,13 @@
+name = input(" what is your name? ")
+
+if len(name) >= 6:
+    print("You name is long")
+elif len(name) == 5:
+    print("Your name is normal")
+else:
+    print("Your name is short")
+
+
 my_list = [1, 4, 3]
 print(4 in my_list)
 print(2 in my_list)
@@ -13,3 +23,6 @@ print(", ".join(lines))
 print("".join(lines))
 print(" ".join(lines))
 print('\n'.join(lines))
+
+
+
