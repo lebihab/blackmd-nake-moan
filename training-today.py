6d321e76c8fbfddaef34dbf3476705d497cc11c5
lebihab <dmha89@gmail.com>
2023-01-11 05:08:41 +0000

    test2

--- training-today.py
+++ training-today.py
@@ -1,1 +1,15 @@
-print("hello world")
+my_list = [1, 4, 3]
+print(4 in my_list)
+print(2 in my_list)
+print(sorted(my_list))
+
+my_sting = "ThIs lEtTer"
+print(my_sting.capitalize())
+print(my_sting.upper())
+
+lines = ['First line', 'Second line', 'Third line']
+print(lines)
+print(", ".join(lines))
+print("".join(lines))
+print(" ".join(lines))
+print('\n'.join(lines))
